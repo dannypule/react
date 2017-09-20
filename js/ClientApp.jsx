c3acd6b54c17d703d71c7a67d5009388479855ba
dannypule <dannypule@dannypules-MacBook-Pro.local>
2017-09-20 18:31:45 +0100

    add spinner and more stuff to details

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,7 +2,17 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+// import Perf from 'react-addons-perf';
 import App from './App';
+
+// window.Perf = Perf; // does not currently work with React fiber
+// Perf.start();
+
+// can also use:
+// Perf.stop()
+// Perf.printWasted()
+// Perf.printInclusive() --- exclusive of lifecycle methods
+// Perf.printExclusive() --- exclusive of lifecycle methods
 
 const renderApp = () => {
   render(<App />, document.getElementById('app'));
